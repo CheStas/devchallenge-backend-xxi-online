ff2aec2bf8c5c6966133747d4bd38e749d95b0df
Stas Chernenko <chstas2@gmail.com>
2024-09-30 22:32:15 +0300

    Setup Nestjs Validation Pipe

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 const { PORT } = process.env;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    disableErrorMessages: false,
+    errorHttpStatusCode: 422,
+  }));
 
   Logger.log(`Starting, PORT:${PORT}`);
 
