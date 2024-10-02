6e06ffa70ca14b05771a89de896ad38ad1171c11
Stas Chernenko <chstas2@gmail.com>
2024-10-02 11:45:33 +0300

    Save Call As File To File Store

--- src/call/call.service.ts
+++ src/call/call.service.ts
@@ -3,12 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 import { CallRepository } from "./call.repository";
 import { CreateCallDto } from "./call.dto";
 import { Call } from "./call.schema";
+import { FileService } from "src/file/file.service";
 
 @Injectable()
 export class CallService {
     constructor(
         private readonly callRepository: CallRepository,
-        private readonly logger: Logger
+        private readonly fileService: FileService,
+        private readonly logger: Logger,
     ) {}
 
     static isCompleted(call: Call): boolean {
@@ -17,33 +19,51 @@ export class CallService {
 
     async getById(id: string): Promise<Call & { isCompleted: boolean }> {
         const result = await this.callRepository.getById(id);
- 
+
         if (!result) {
             throw new NotFoundException('Unknown call id');
         }
 
-        return {
+        const fileResponse = {
             isCompleted: CallService.isCompleted(result),
             id: result.id,
             name: result.name,
             location: result.location,
             emotional_tone: result.emotional_tone,
             text: result.text,
+            fileId: result.fileId,
+            url: result.url,
         }
+
+        this.logger.log(`fileResponse: ${JSON.stringify(fileResponse, null, 2)}`);
+
+        return fileResponse;
     }
 
     async create(call: CreateCallDto): Promise<{ id: string }> {
         const uuid = uuidv4();
+        // we are sure here that url is valid, it's validated before
+        this.logger.log(`Saving the file uuid: ${ uuid }, call: ${ JSON.stringify(call, null, 2) }`);
+        const saveFileResult = await this.fileService.saveFile(uuid, call.audio_url);
+        this.logger.log(`Save File Result: ${ JSON.stringify(saveFileResult, null, 2) }`);
+
         const result = await this.callRepository.create({
             id: uuid,
+            fileId: saveFileResult.fileId,
+            url: call.audio_url,
         });
 
-        return {
+        const createdCall = {
             id: result.id,
             name: result.name, 
             location: result.location,
             emotional_tone: result.emotional_tone,
             text: result.text,
+            fileId: result.fileId,
         }
+
+        this.logger.log(`createdCall: ${JSON.stringify(createdCall, null, 2)}`);
+
+        return { id: result.id,};
     }
 }
